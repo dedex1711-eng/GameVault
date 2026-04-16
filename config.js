ef970cde4758ac87cff029818d6a308160080262
dedex1711-eng <dedex1711@gmail.com>
2026-04-15 23:54:09 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+var JSONBIN_BIN_ID  = "69e04e2f856a6821893be52a";
+var JSONBIN_API_KEY = "$2a$10$IqA7FYBhSf6dGSTOJ3X5fOwR/ynzYk1Nz4i80Djr6il.DMK9k74Qa";
+var JSONBIN_URL     = "https://api.jsonbin.io/v3/b/" + JSONBIN_BIN_ID;
